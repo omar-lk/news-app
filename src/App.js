e68c2d39e4b43282d8b515f5a7659dace3f248fa
omar lekouch <omar.lekouch@lincx.la>
2024-05-21 12:58:08 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { useEffect } from 'react'
+import './App.css'
+import i18next from 'i18next'
 
-function App() {
+// Initialize i18next outside of the component
+i18next.init({
+  lng: 'en',
+  resources: {
+    ar: {
+      translation: {
+        Translate: 'الترجمة'
+      }
+    },
+    en: {
+      translation: {
+        Translate: 'translation'
+      }
+    }
+  }
+})
+
+function App () {
+  useEffect(() => {
+    let fromLang = 'en'
+    let toLang = 'ar' // translate to norwegian
+    let text = 'something to translate'
+
+    const API_KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+
+    let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`
+    url += '&q=' + encodeURI(text)
+    url += `&source=${fromLang}`
+    url += `&target=${toLang}`
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log('response from google: ', response)
+      })
+      .catch(error => {
+        console.log('There was an error with the translation request: ', error)
+      })
+  }, [])
+  const changeLanguage = lng => {
+    i18next.changeLanguage(lng)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={() => changeLanguage('ar')}>arabic</button>
+        {i18next.t('Translate')}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
